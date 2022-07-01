ea92a801787cf6ee5cd440dffe716fdc3ff1984a
Ebenyi Theophilus <ebenyitheophilusodo@gmail.com>
2022-06-30 21:00:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-/
-
+/**
  * main - Entry point
  *
  *  * Return: Always 0 (sucess)
@@ -9,11 +8,7 @@
  *   */
 
  int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
