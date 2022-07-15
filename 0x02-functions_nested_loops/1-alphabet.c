43248b5c8bb7d2b6cab900e5a9f84583d9a1ecef
Ebenyi Theophilus <ebenyitheophilusodo@gmail.com>
2022-07-15 00:54:03 -0700

    _putchr

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * print_alphabet - function to print abc
- *
- * Return: 0
+ *print_alphabet - function that print alphabet
+ *it prints in lowercase
+ *Return: 0
  */
 void print_alphabet(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		 _putchar(c);
-	}
-_putchar('\n');
+int letter
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(c);
+}
+_putchar(10);
 }
