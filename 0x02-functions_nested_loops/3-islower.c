82697577c5ca6b4b4f2cb24f8bc36b55e12e548a
Ebenyi Theophilus <ebenyitheophilusodo@gmail.com>
2022-07-15 03:14:12 -0700

    islow

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 /**
-*main - prints sum of even fibonacci
-*suit elements
-*fibonacci suit numbers
-*Return: return 0
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
 */
-int main(void)
+int _islower(int c)
 {
-unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
-while (n3 <= 4000000)
-{
-n3 = n1 + n2;
-n1 = n2;
-n2 = n3;
+if (c >= 'a' && c <= 'z')
+return (1);
 
-if ((n1 % 2) == 0)
-sum += n1;
-
-}
-printf("%ld\n", sum);
 return (0);
 }
